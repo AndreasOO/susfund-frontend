382c4a860698a74736129a5d17bcdaf7f1082787
Andreas Ohlander <andreas.ohlander@gmail.com>
2025-03-17 20:30:09 +0100

    Added navbar and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {GeneralOverviewComponent} from './case-overview/general-overview/general-overview.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'overview', component: GeneralOverviewComponent},
+  {path: '', component: GeneralOverviewComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
