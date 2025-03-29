dba5b1b47a1bb8a947c86331f13ce76f3c38e791
Josefin Törner <josefintorner@gmail.com>
2025-03-29 14:06:25 +0100

    case-application component: ngOnInit loads a map of section and questions with their answers and added html-cards

--- src/app/case-view/case-application/case-application.component.ts
+++ src/app/case-view/case-application/case-application.component.ts
@@ -1,6 +1,9 @@
 import {Component, inject, Signal} from '@angular/core';
 import {ROUTER_OUTLET_DATA} from '@angular/router';
 import {CaseDetails} from '../../cases-services/case-entity/case-details';
+import {ApplicationQuestion} from '../../cases-services/case-entity/application-question';
+import {OnInit} from '@angular/core';
+import {ApplicationSection} from '../../cases-services/case-entity/application-section';
 
 @Component({
   selector: 'app-case-application',
@@ -8,6 +11,23 @@ import {CaseDetails} from '../../cases-services/case-entity/case-details';
   templateUrl: './case-application.component.html',
   styleUrl: './case-application.component.css'
 })
-export class CaseApplicationComponent {
+export class CaseApplicationComponent implements OnInit {
   caseDetails = inject(ROUTER_OUTLET_DATA) as Signal<CaseDetails>
+  public sections: Map<String, Map<ApplicationQuestion, String>> = new Map();
+
+  ngOnInit() {
+    this.caseDetails().caseApplication?.questionResults?.forEach(questionResult => {
+      const currentSection = questionResult.applicationQuestion.applicationSection;
+      const question = questionResult.applicationQuestion;
+      const answer = questionResult.answer;
+
+      if (!this.sections.has(currentSection.name)) {
+        this.sections.set(currentSection.name, new Map<ApplicationQuestion, string>());
+      }
+
+      this.sections.get(currentSection.name)?.set(question, answer);
+    })
+
+  }
 }
+
