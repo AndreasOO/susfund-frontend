9e4b78c154b395a129242b057d4722ed3e912590
Andreas Ohlander <andreas.ohlander@gmail.com>
2025-03-31 11:29:42 +0200

    Added basic layout for budget. Still needs interactivity and possibility to add new posts.

--- src/app/case-view/case-budget/case-budget.component.ts
+++ src/app/case-view/case-budget/case-budget.component.ts
@@ -1,6 +1,7 @@
 import {Component, inject, Signal} from '@angular/core';
 import {ROUTER_OUTLET_DATA} from '@angular/router';
 import {CaseDetails} from '../../cases-services/case-entity/case-details';
+import {pipe} from "rxjs";
 
 @Component({
   selector: 'app-case-budget',
@@ -10,4 +11,8 @@ import {CaseDetails} from '../../cases-services/case-entity/case-details';
 })
 export class CaseBudgetComponent {
   caseDetails = inject(ROUTER_OUTLET_DATA) as Signal<CaseDetails>
+
+  getTotal():number {
+    return this.caseDetails().caseBudget.budgetPosts.map(post => post.estimatedCost).reduce((a,b) => a+b);
+  }
 }
