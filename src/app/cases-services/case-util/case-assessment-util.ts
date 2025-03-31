27cbe4624d333a9b074039bf2b06e020ec9c00ba
Andreas Ohlander <andreas.ohlander@gmail.com>
2025-03-31 15:49:40 +0200

    Added service for assessment util and implementet in assessment

--- src/app/cases-services/case-util/case-assessment-util.ts
+++ src/app/cases-services/case-util/case-assessment-util.ts
@@ -2,5 +2,5 @@ import {AssessmentResult} from '../case-entity/assessment-result';
 
 export interface CaseAssessmentUtil {
   caseAssessmentId:number,
-  assessmentResults:AssessmentResult[]
+  assessmentResults:AssessmentResult[][]
 }
