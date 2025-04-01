ea9aecc85e594e0dbe6d50d4125b938806187133
Andreas Ohlander <andreas.ohlander@gmail.com>
2025-04-01 21:08:32 +0200

    Corrected variable from assisting_text to assistingText

--- src/app/cases-services/case-entity/application-question.ts
+++ src/app/cases-services/case-entity/application-question.ts
@@ -4,6 +4,6 @@ export interface ApplicationQuestion {
   id:number,
   title:string,
   preamble:string,
-  assisting_text:string,
+  assistingText:string,
   applicationSection:ApplicationSection
 }
