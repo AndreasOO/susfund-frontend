0fccad11da40ab880e8cce0f890efdc03a80914c
Josefin Törner <josefintorner@gmail.com>
2025-04-03 17:12:47 +0200

    interface for caseApplicationUtil, added fetcher-method in services, updated case-application component to retrieve a caseApplicationUtil object

--- src/app/case-view/case-application/case-application.component.ts
+++ src/app/case-view/case-application/case-application.component.ts
@@ -1,10 +1,12 @@
 import {Component, inject, Signal} from '@angular/core';
-import {ROUTER_OUTLET_DATA} from '@angular/router';
+import {Router, ROUTER_OUTLET_DATA} from '@angular/router';
 import {CaseDetails} from '../../cases-services/case-entity/case-details';
 import {ApplicationQuestion} from '../../cases-services/case-entity/application-question';
 import {OnInit} from '@angular/core';
 import {ApplicationSection} from '../../cases-services/case-entity/application-section';
 import {QuestionResults} from '../../cases-services/case-entity/question-results';
+import {CasesFetcherService} from '../../cases-services/cases-fetcher.service';
+import {CaseApplicationUtil} from '../../cases-services/case-util/case-application-util';
 
 @Component({
   selector: 'app-case-application',
@@ -14,16 +16,17 @@ import {QuestionResults} from '../../cases-services/case-entity/question-results
 })
 export class CaseApplicationComponent implements OnInit {
   caseDetails = inject(ROUTER_OUTLET_DATA) as Signal<CaseDetails>
-  public questions: QuestionResults[][] | undefined;
+
+  public caseId:string | undefined
+  public caseApplicationUtil: CaseApplicationUtil | undefined;
+
+  constructor(public router:Router, public fetcher:CasesFetcherService) {
+  }
 
   ngOnInit() {
 
-    this.questions = [...new Set(this.caseDetails()
-      .caseApplication?.questionResults.map(result => result.applicationQuestion?.applicationSection.id))]
-      .map(section => {
-        return (this.caseDetails().caseApplication?.questionResults ?? [])
-          .filter(questionResult => questionResult.applicationQuestion?.applicationSection.id === section)
-      });
+    this.caseId = this.router.url.split("/")[this.router.url.split("/").indexOf("cases")+1];
+    this.fetcher.getApplicationUtilByCaseId(this.caseId).subscribe(caseApplicationUtil => this.caseApplicationUtil = caseApplicationUtil)
 
   }
 }
