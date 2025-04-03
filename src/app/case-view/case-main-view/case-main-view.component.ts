0bd4b76245d95d69f29337f141db6ef91d1c7959
Josefin Törner <josefintorner@gmail.com>
2025-04-03 16:38:39 +0200

    changes to ngOnInit in case main view

--- src/app/case-view/case-main-view/case-main-view.component.ts
+++ src/app/case-view/case-main-view/case-main-view.component.ts
@@ -31,14 +31,12 @@ export class CaseMainViewComponent implements OnInit {
   ngOnInit() {
     this.caseId = this.router.url.split("/")[this.router.url.split("/").indexOf("cases")+1];
     this.fetcher.getCaseById(this.caseId).subscribe(caseDetails => this.caseDetails = caseDetails!,
-
-
-      err => { if  (err.status === 0) {
-        console.log(err.status);
-        this.router.navigateByUrl("/login")
-      }
-      },
-      () => console.log("finished"))
+                                                                              err => { if  (err.status === 0) {
+                                                                                console.log(err.status);
+                                                                                this.router.navigateByUrl("/login")
+                                                                              }
+                                                                              },
+                                                                              () => console.log("finished"))
     console.log(this.caseDetails)
   }
 
