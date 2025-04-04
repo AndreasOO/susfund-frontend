befdba4bdc2423d54a5c8b42f7d96a5840181127
Andreas Ohlander <andreas.ohlander@gmail.com>
2025-04-04 21:53:31 +0200

    Added fetcher methods and impl in case overview component. Also adjusted grid layout

--- src/app/case-view/case-overview/case-overview.component.ts
+++ src/app/case-view/case-overview/case-overview.component.ts
@@ -1,6 +1,8 @@
-import {Component, inject, Input, Signal} from '@angular/core';
+import {Component, inject, Input, Signal, OnInit} from '@angular/core';
 import {CaseDetails} from '../../cases-services/case-entity/case-details';
 import {Router, ROUTER_OUTLET_DATA} from '@angular/router';
+import {CasesFetcherService} from '../../cases-services/cases-fetcher.service';
+import {CaseManager} from '../../cases-services/case-entity/case-manager';
 
 
 @Component({
@@ -9,11 +11,21 @@ import {Router, ROUTER_OUTLET_DATA} from '@angular/router';
   templateUrl: './case-overview.component.html',
   styleUrl: './case-overview.component.css'
 })
-export class CaseOverviewComponent {
-  caseDetails = inject(ROUTER_OUTLET_DATA) as Signal<CaseDetails>
-  constructor(public router:Router) {
+export class CaseOverviewComponent implements OnInit{
+
+  caseId:string | undefined
+  currentCaseManager:CaseManager | undefined
+  caseManagerList:CaseManager[] | undefined
+  caseDetails:CaseDetails | undefined
+
+  constructor(public router:Router, private fetcher:CasesFetcherService) {
 
   }
 
-  // @Input() caseDetails:CaseDetails|undefined;
+  ngOnInit() {
+    this.caseId = this.router.url.split("/")[this.router.url.split("/").indexOf("cases")+1];
+    this.fetcher.getCaseById(this.caseId).subscribe(caseDetails => this.caseDetails = caseDetails!)
+    this.fetcher.getCaseManagerByCaseId(this.caseId).subscribe(caseManager => this.currentCaseManager = caseManager!)
+    this.fetcher.getAllCaseManagers().subscribe(caseManagerList => this.caseManagerList = caseManagerList!)
+  }
 }
