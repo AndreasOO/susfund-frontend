2386cf0a9e2db05656b4e49ab062baa8069ac485
Andreas Ohlander <andreas.ohlander@gmail.com>
2025-04-06 21:36:22 +0200

    Added new fetcher method and implemented related cases to org in org view

--- src/app/case-view/case-organization-details/case-organization-details.component.ts
+++ src/app/case-view/case-organization-details/case-organization-details.component.ts
@@ -3,6 +3,8 @@ import {Router, ROUTER_OUTLET_DATA} from '@angular/router';
 import {CaseDetails} from '../../cases-services/case-entity/case-details';
 import {Organization} from '../../cases-services/case-entity/organization';
 import {CasesFetcherService} from '../../cases-services/cases-fetcher.service';
+import {CaseLazy} from '../../cases-services/case-entity/case-lazy';
+import {async, flatMap, mergeMap} from 'rxjs';
 
 @Component({
   selector: 'app-case-organization-details',
@@ -13,6 +15,7 @@ import {CasesFetcherService} from '../../cases-services/cases-fetcher.service';
 export class CaseOrganizationDetailsComponent implements OnInit{
   caseId: string | undefined
   caseOrganization:Organization | undefined
+  casesRelatedToOrganization:CaseLazy[] | undefined
 
   constructor(public router:Router, public fetcher:CasesFetcherService) {
   }
@@ -20,5 +23,6 @@ export class CaseOrganizationDetailsComponent implements OnInit{
   ngOnInit() {
     this.caseId = this.router.url.split("/")[this.router.url.split("/").indexOf("cases")+1];
     this.fetcher.getOrganizationByCaseId(this.caseId).subscribe(caseOrganization => this.caseOrganization = caseOrganization!)
+    this.fetcher.getCasesLazyByCaseOrganization(this.caseId).subscribe(caseList => this.casesRelatedToOrganization = caseList!)
   }
 }
